1af0fc59d84a41544253dfc9ce1aa8e3294ee637
thoyyibtvt <mtpt1.muhammed@gmail.com>
2026-02-18 23:40:01 +0530

    work started

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT} on all interfaces`);
 });
+
